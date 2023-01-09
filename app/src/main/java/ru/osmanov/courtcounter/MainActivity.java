6e6c491a44443f77472e3a61cd0fd0b9eb2b1737
Nurulla Osmanov <Shivaii@yandex.ru>
2023-01-09 15:35:11 +0300

    Добавил сохранение и восстановление данных при пересоздании Активити

--- app/src/main/java/ru/osmanov/courtcounter/MainActivity.java
+++ app/src/main/java/ru/osmanov/courtcounter/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.osmanov.courtcounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,48 +11,56 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTeamA;
     private TextView textViewTeamB;
     private Button button1TeamA, button2TeamA, button3TeamA, button1TeamB, button2TeamB, button3TeamB, reset;
-    private int countTeamA;
-    private int countTeamB;
+    Counter countTeamA;
+    Counter countTeamB;
+
+    private final static String keyCounterA = "CounterA";
+    private final static String keyCounterB = "CounterB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countTeamA = new Counter();
+        countTeamB = new Counter();
         initView();
         setOnClickButton();
     }
 
+
     private void setOnClickButton() {
+
         button1TeamA.setOnClickListener(view -> {
-            countTeamA++;
-            textViewTeamA.setText(String.valueOf(countTeamA));
+            countTeamA.setCountA(countTeamA.getCountA() + 1);
+            textViewTeamA.setText(String.valueOf(countTeamA.getCountA()));
         });
         button2TeamA.setOnClickListener(view -> {
-            countTeamA += 2;
-            textViewTeamA.setText(String.valueOf(countTeamA));
+            countTeamA.setCountA(countTeamA.getCountA() + 2);
+            textViewTeamA.setText(String.valueOf(countTeamA.getCountA()));
         });
         button3TeamA.setOnClickListener(view -> {
-            countTeamA += 3;
-            textViewTeamA.setText(String.valueOf(countTeamA));
+            countTeamA.setCountA(countTeamA.getCountA() + 3);
+            textViewTeamA.setText(String.valueOf(countTeamA.getCountA()));
         });
         button1TeamB.setOnClickListener(view -> {
-            countTeamB++;
-            textViewTeamB.setText(String.valueOf(countTeamB));
+            countTeamB.setCountB(countTeamB.getCountB() + 1);
+            textViewTeamB.setText(String.valueOf(countTeamB.getCountB()));
         });
         button2TeamB.setOnClickListener(view -> {
-            countTeamB += 2;
-            textViewTeamB.setText(String.valueOf(countTeamB));
+            countTeamB.setCountB(countTeamB.getCountB() + 2);
+            textViewTeamB.setText(String.valueOf(countTeamB.getCountB()));
         });
         button3TeamB.setOnClickListener(view -> {
-            countTeamB += 3;
-            textViewTeamB.setText(String.valueOf(countTeamB));
+            countTeamB.setCountB(countTeamB.getCountB() + 3);
+            textViewTeamB.setText(String.valueOf(countTeamB.getCountB()));
         });
 
         reset.setOnClickListener(view -> {
-            countTeamA = 0;
-            countTeamB = 0;
-            textViewTeamA.setText(String.valueOf(countTeamA));
-            textViewTeamB.setText(String.valueOf(countTeamB));
+            countTeamA.setCountA(0);
+            countTeamB.setCountB(0);
+            textViewTeamA.setText(String.valueOf(countTeamA.getCountA()));
+            textViewTeamB.setText(String.valueOf(countTeamB.getCountB()));
         });
     }
 
@@ -65,5 +74,22 @@ public class MainActivity extends AppCompatActivity {
         button2TeamB = findViewById(R.id.btn_2points_teamB);
         button3TeamB = findViewById(R.id.btn_3points_teamB);
         reset = findViewById(R.id.btn_reset);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(keyCounterA, countTeamA);
+        outState.putParcelable(keyCounterB, countTeamB);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        countTeamA = savedInstanceState.getParcelable(keyCounterA);
+        textViewTeamA.setText(String.valueOf(countTeamA.getCountA()));
+        countTeamB = savedInstanceState.getParcelable(keyCounterB);
+        textViewTeamB.setText(String.valueOf(countTeamB.getCountB()));
+
     }
 }
